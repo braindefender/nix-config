54697d084415696cabf54612c7abc226811810f8
Nikita Shirokov <braindefender@gmail.com>
2023-11-30 16:14:14 +0700

    feat: add dunst

---
+++ modules/nixos/apps/dunst/default.nix
@@ -0,0 +1,16 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.plusultra;
+
+let cfg = config.plusultra.apps.dunst;
+
+in {
+  options.plusultra.apps.dunst = with types; {
+    enable = mkBoolOpt false "Enable dunst?";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ dunst ];
+  };
+}
