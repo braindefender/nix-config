5a73b3e9c2016f0f79aff463216a44b5af002f77
Nikita Shirokov <braindefender@gmail.com>
2024-03-29 13:23:12 +0700

    feat: add vivaldi

---
+++ modules/nixos/apps/vivaldi/default.nix
@@ -0,0 +1,18 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.plusultra;
+
+let
+  cfg = config.plusultra.apps.vivaldi;
+in
+{
+  options.plusultra.apps.vivaldi = with types; {
+    enable = mkBoolOpt false "Enable Vivaldi?";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ vivaldi ];
+  };
+}
+
