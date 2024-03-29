3cff3001db91fa768c518aeeedb6ece699d75d1b
Nikita Shirokov <braindefender@gmail.com>
2024-03-30 04:55:02 +0700

    fix: update xkb layouts record

--- modules/nixos/hardware/universal-layout/default.nix
+++ modules/nixos/hardware/universal-layout/default.nix
@@ -33,7 +33,7 @@ in
   };
 
   config = mkIf cfg.enable {
-    services.xserver.extraLayouts = layouts;
+    services.xserver.xkb.extraLayouts = layouts;
 
     environment.systemPackages = with pkgs; [ numlockx ];
     # services.udev.packages = [
