f5eeddd2065e0c97866063c35e0c56eed2cf6b93
Nikita Shirokov <braindefender@gmail.com>
2024-03-29 13:21:37 +0700

    feat: add zellij

---
+++ modules/nixos/tools/zellij/default.nix
@@ -0,0 +1,22 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.plusultra;
+
+let cfg = config.plusultra.tools.zellij;
+
+in {
+  options.plusultra.tools.zellij = with types; {
+    enable = mkBoolOpt false "Enable Zellij?";
+  };
+
+  config = mkIf cfg.enable {
+		plusultra.system.home.extraOptions = {
+			programs.zellij = {
+				enable = true;
+
+				enableZshIntegration = true;
+			};
+		};
+  };
+}
