bfea966c630b58275d3a6dd94b5ec7e4d5cdcd6f
Nikita Shirokov <braindefender@gmail.com>
2024-03-29 13:25:11 +0700

    feat: add staging acme

---
+++ modules/nixos/services/acme/default.nix
@@ -0,0 +1,48 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.plusultra;
+
+let
+  cfg = config.plusultra.services.acme;
+  domain = config.networking.domain;
+in
+{
+  options.plusultra.services.acme = with types; {
+    enable = mkBoolOpt false "Enable ACME?";
+  };
+
+  config = mkIf cfg.enable {
+    security.acme = {
+      acceptTerms = true;
+
+      defaults = {
+        email = config.plusultra.user.email;
+        # use staging server for testing purpose. higher rate limits.
+        server = "https://acme-staging-v02.api.letsencrypt.org/directory";
+      };
+
+      certs = {
+        "${domain}" = {
+          extraDomainNames = [ "*.${domain}" ];
+          dnsProvider = "regru";
+          group = "nginx";
+
+          credentialFiles = {
+            REGRU_USERNAME_FILE = "/home/brain/regru/username";
+            REGRU_PASSWORD_FILE = "/home/brain/regru/password";
+          };
+        };
+      };
+    };
+
+    services.nginx = {
+      enable = true;
+
+      recommendedTlsSettings = true;
+      recommendedGzipSettings = true;
+      recommendedOptimisation = true;
+      recommendedProxySettings = true;
+    };
+  };
+}
